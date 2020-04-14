094dbe6e8178db3e0c3a2af34619f8de01b2f7d1
keyao21 <k.yyao7@gmail.com>
2020-04-13 23:34:15 -0400

    updating experiment params for dg, improving error checks for interp.py, finishing compare_trajectories.py

--- QG_FTLE/src/compare_trajectories.py
+++ QG_FTLE/src/compare_trajectories.py
@@ -11,19 +11,20 @@ import interp
 from config import * 
 
 
-def generate_single_trajectory( velocity_func_fullpaths, initial_conditions, 
-                                elapsed_time, dt,  ):
+def generate_single_trajectory( velocity_func_filenames, initial_conditions, 
+                                elapsed_time, dt):
     """
     Create trajectory plots for single intiial condition
     Loads stream function file and calculate trajectories
     using RK4
     
-    velocity_func_filename: list of string filename(s)
+    velocity_func_filenames: list of string filename(s)
     initial_conditions: tuple of initial condition coordinates
     """
     num_samples = 1
     vfuncs_list = [] 
-    for velocity_func_fullpath in velocity_func_fullpaths:
+    for velocity_func_filename in velocity_func_filenames:
+        velocity_func_fullpath = os.path.join( INTERP_VELOCITY_PATH_DIR, velocity_func_filename)
         vfuncs_list.append( util.load_velocity_field(velocity_func_fullpath) )
         # TODO: MULTIPLE VFUNCS PER VELOCITY_FUNC_FULLPATH
         #!!!!
@@ -31,11 +32,16 @@ def generate_single_trajectory( velocity_func_fullpaths, initial_conditions,
     # Generate estimated trajectories
     # state : numpy array shaped as ...
     # ( num velocity_funcs, num_samples, time steps, num coordinates)
-    state = np.zeros(( len(velocity_func_fullpaths), num_samples, int(elapsed_time/dt)+1, 2 ))
+    state = np.zeros(( len(velocity_func_filenames), num_samples, int(elapsed_time/dt)+1, 2 ))
     for vfunc_idx, vfuncs in enumerate(vfuncs_list):
         state[vfunc_idx,:,0,0] = initial_conditions[0]
         state[vfunc_idx,:,0,1] = initial_conditions[1]
         for i,t in enumerate(np.arange(0, elapsed_time, dt)):
+            # import pdb;pdb.set_trace()
+
+            # print( state[vfunc_idx,:,(i-max(5,i)):i,:] )
+            
+
             state[vfunc_idx,:,i+1,:] = interp.rk4(vfuncs, state[vfunc_idx,:,i,:].copy(), t, dt)
         print('Trajectories finished.')
 
@@ -62,7 +68,21 @@ def compare_single_trajectories(  ):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--traj', '--trajectories', '-t', type=int)
+    parser.add_argument('--test', type=int)
     args = parser.parse_args()
+
+    if args.test: 
+        velocity_func_filenames = ['dgsf_0p1_128_64_0p1_0p25000_2.0.uvinterp.est', 'dgsf_0p1_128_64_0p1_0p25000_2.0.uvinterp.actual']
+        # velocity_func_filenames = ['QGds02di02dm02p3_1000_1.4.uvinterp.est', 'QGds02di02dm02p3_1000_1.4.uvinterp.actual']
+        initial_conditions = [1.25, 0.25]
+        elapsed_time = 250
+        dt = 0.05
+        generate_single_trajectory(velocity_func_filenames=velocity_func_filenames, initial_conditions=initial_conditions,
+                                   elapsed_time=elapsed_time, dt=dt )
+
+
+
+
 
 
 
--- scripts/run_dg_experiments.py
+++ scripts/run_dg_experiments.py
@@ -46,22 +46,23 @@ def generate_experiment_id():
     # from experiment parmas create unique string to be used as key for config dicts 
     pass
 
-def generate_ml_fluid_params(  training_length, trained_model_params, testing_length, trained_model_filename, stream_function_prefix ): 
+def generate_ml_fluid_params(   training_length, trained_model_params, testing_length, trained_model_filename, stream_function_prefix,
+                                dt=0.1, elapsedTime=2500, xct=128, yct=64, amp=0.1, epsilon=0.2 ): 
     # params to pass into config for ML_Fluid 
     params_dict = {
         'PREPROCESS' : { 
-            'sf_filename' : 'dgsf_0p1_128_64_0p1_0p2',
+            'sf_filename' : f'dgsf_{dt}_{xct}_{yct}_{amp}_{epsilon}',
             'training_length'         : training_length,
-            'training_data_filename'  : 'dgsf_0p1_128_64_0p1_0p2.TRAIN',
-            'testing_data_filename'  : 'dgsf_0p1_128_64_0p1_0p2.TEST'  
+            'training_data_filename'  : f'dgsf_{dt}_{xct}_{yct}_{amp}_{epsilon}.TRAIN',
+            'testing_data_filename'  : f'dgsf_{dt}_{xct}_{yct}_{amp}_{epsilon}.TEST'  
         },
         'TRAIN' : { 
-            'training_data_filename'  : 'dgsf_0p1_128_64_0p1_0p2.TRAIN',
+            'training_data_filename'  : f'dgsf_{dt}_{xct}_{yct}_{amp}_{epsilon}.TRAIN',
             'trained_model_params'    : trained_model_params,
             'trained_model_filename'  : trained_model_filename,
         },
         'TEST' : { 
-            'testing_data_filename'   : 'dgsf_0p1_128_64_0p1_0p2.TEST',
+            'testing_data_filename'   : f'dgsf_{dt}_{xct}_{yct}_{amp}_{epsilon}.TEST',
             'testing_length'          : testing_length,
             'trained_model_filename'  : trained_model_filename, 
             'output_filenames'        : { # Davis wuz here!!
@@ -70,18 +71,18 @@ def generate_ml_fluid_params(  training_length, trained_model_params, testing_le
             }
         },
         'GENERATE_STREAM_FUNCTION_FIELDS' : {
-            'dt' : 0.1, 
-            'elapsedTime' : 2500, 
-            'xct': 128, 
-            'yct': 64,
-            'amp': 0.1,
-            'epsilon': 0.2,
-            'stream_function_filename' : 'dgsf_0p1_128_64_0p1_0p2'
+            'dt' : dt, 
+            'elapsedTime' : elapsedTime, 
+            'xct': xct, 
+            'yct': yct,
+            'amp': amp,
+            'epsilon': epsilon,
+            'stream_function_filename' : f'dgsf_{dt}_{xct}_{yct}_{amp}_{epsilon}'
         }
     }
     return params_dict
 
-def generate_qgftle_params( stream_function_prefix ):
+def generate_qgftle_params( stream_function_prefix, mapped_dt=20, dt=0.1, iters=10, xct=128, yct=64 ):
     """
     params to pass into config for QG_FTLE
     stream_function_prefix - part of the stream function file name 
@@ -99,31 +100,32 @@ def generate_qgftle_params( stream_function_prefix ):
                 'velocity_func_filename' : f"{stream_function_prefix}.uvinterp.{actual_flag}"
             }, 
             'GENERATE_FTLE_MAPPING' : {
-                'iters' : 100, 
-                'mapped_dt' : 20,
-                'dt' : 0.1,
-                'xct': 128, 
-                'yct': 64,
+                'iters' : iters, 
+                'mapped_dt' : mapped_dt,
+                'dt' : dt,
+                'xct': xct, 
+                'yct': yct,
                 # Davis wuz here!!
                 'velocity_func_filename': f"{stream_function_prefix}.uvinterp.{actual_flag}",
                 'mapping_path_dir': f"{stream_function_prefix}.{actual_flag}"
             },
             'GENERATE_FTLE_FIELDS': {
-                'iters' : 100,
-                'xct': 128, 
-                'yct': 64,
+                'iters' : iters,
+                'xct': xct, 
+                'yct': yct,
                 'mapping_path_dir': f"{stream_function_prefix}.{actual_flag}",
                 'ftle_path_dir' : f"{stream_function_prefix}.{actual_flag}"
             }, 
             'GENERATE_FTLE_ANIMATIONS': {
-                'iters' : 100,
-                'xct': 128, 
-                'yct': 64,
+                'iters' : iters,
+                'xct': xct, 
+                'yct': yct,
                 'ftle_path_dir' : f"{stream_function_prefix}.{actual_flag}",
                 'ftle_animation_filename' : f"{stream_function_prefix}.{actual_flag}.gif",
             }
         }
     return params_dict
+
 
 def run_experiment(resSize, spectral_radius): 
     ## Preparing parameters for entire experiment
@@ -141,18 +143,28 @@ def run_experiment(resSize, spectral_radius):
         "ridgeReg"          : 0.01, 
         "mute"              : False 
     }
-    training_length = 12000
-    testing_length = 12000
-    stream_function_prefix = f"dgsf_0p1_128_64_0p1_0p2{resSize}_{spectral_radius:.1f}"
+    training_length = 10000
+    testing_length = 10000
+    dt = 0.05
+    elapsedTime = 2100
+    xct = 128
+    yct = 64
+    amp = 0.1
+    epsilon = 0.2
+    stream_function_prefix = f"dgsf_{dt}_{xct}_{yct}_{amp}_{epsilon}_{resSize}_{spectral_radius:.1f}"
     trained_model_filename = f'{stream_function_prefix}.ESN'
     ml_fluid_params_dict = generate_ml_fluid_params(    training_length=training_length, 
                                         trained_model_params=trained_model_params,
                                         testing_length=testing_length,
                                         trained_model_filename=trained_model_filename,
-                                        stream_function_prefix=stream_function_prefix
+                                        stream_function_prefix=stream_function_prefix,
+                                        dt=dt, elapsedTime=elapsedTime, xct=xct, yct=yct,
+                                        amp=amp, epsilon=epsilon
                                     )
-    qgftle_params_dict = generate_qgftle_params( stream_function_prefix=stream_function_prefix )
-
+    mapped_dt = 20
+    iters = 10
+    qgftle_params_dict = generate_qgftle_params( stream_function_prefix=stream_function_prefix,
+                                                 mapped_dt=mapped_dt, dt=dt, iters=iters, xct=xct,yct=yct )
     """
     0. Generate double gyre raw data (src code is located in QG_FTLE directory)
     Transfer raw data file to ML_Fluids directory for step 1. 
@@ -227,6 +239,130 @@ def run_experiment(resSize, spectral_radius):
     switch_to_home_dir()
     return ssi 
 
+
+
+
+def run_experiment_without_ftle(resSize, spectral_radius): 
+    ## Preparing parameters for entire experiment
+    ## ml fluid params should link to qgftle params by  
+    ## stream_function_estimated and stream_function_actual
+    trained_model_params = { 
+        "initLen"           : 0, 
+        "resSize"           : resSize, 
+        "partial_know"      : False, 
+        "noise"             : 1e-2, 
+        "density"           : 1e-1, 
+        "spectral_radius"   : spectral_radius, 
+        "leaking_rate"      : 0.2, 
+        "input_scaling"     : 0.3, 
+        "ridgeReg"          : 0.01, 
+        "mute"              : False 
+    }
+    training_length = 10000
+    testing_length = 10000
+    dt = 0.05
+    elapsedTime = 2100
+    xct = 128
+    yct = 64
+    amp = 0.1
+    epsilon = 0.2
+    stream_function_prefix = f"dgsf_{dt}_{xct}_{yct}_{amp}_{epsilon}_{resSize}_{spectral_radius:.1f}"
+    trained_model_filename = f'{stream_function_prefix}.ESN'
+    ml_fluid_params_dict = generate_ml_fluid_params(    training_length=training_length, 
+                                        trained_model_params=trained_model_params,
+                                        testing_length=testing_length,
+                                        trained_model_filename=trained_model_filename,
+                                        stream_function_prefix=stream_function_prefix,
+                                        dt=dt, elapsedTime=elapsedTime, xct=xct, yct=yct,
+                                        amp=amp, epsilon=epsilon
+                                    )
+    mapped_dt = 20
+    iters = 10
+    qgftle_params_dict = generate_qgftle_params( stream_function_prefix=stream_function_prefix,
+                                                 mapped_dt=mapped_dt, dt=dt, iters=iters, xct=xct,yct=yct )
+
+
+    """
+    0. Generate double gyre raw data (src code is located in QG_FTLE directory)
+    Transfer raw data file to ML_Fluids directory for step 1. 
+    """
+    # ensure correct directory
+    switch_to_qgftle_src_dir()
+    import double_gyre
+    double_gyre.generate_streamfunction_values(**ml_fluid_params_dict['GENERATE_STREAM_FUNCTION_FIELDS'] )
+    switch_to_home_dir()
+    ## COPY DOUBLE GYRE STREAM FUNCTION FILES OVER TO ML_FLUIDS INPUT DIRECTORY    
+    dg_raw_streamfunction_filename = ml_fluid_params_dict['GENERATE_STREAM_FUNCTION_FIELDS']['stream_function_filename']
+    dg_raw_streamfunction_fullpath = os.path.join(QG_FTLE_INPUTS_fullpath, dg_raw_streamfunction_filename)
+    shutil.copy(dg_raw_streamfunction_fullpath, ML_Fluid_raw_inputs_fullpath)
+
+
+    """
+    1. Running ML_Fluids procedure - train a model on training data and generate 
+    and testing sample to compare. Specifically, we must
+        a. Preprocess data 
+        b. train data 
+        c. test data 
+    """
+    # ensure correct directory
+    switch_to_mlfluids_src_dir()
+    print(os.getcwd())
+    # a. preprocessing data
+    import preprocess
+    preprocess.preprocess_numpy_input_data( **ml_fluid_params_dict['PREPROCESS'] )
+    # b. training data
+    import train 
+    train.train_ESN(**ml_fluid_params_dict['TRAIN'])
+    # c. testing data 
+    import test 
+    test.test_ESN(**ml_fluid_params_dict['TEST'])
+    switch_to_home_dir()
+
+    ## COPY STREAM FUNCTION FILES OVER TO QG_FTLE INPUT DIRECTORY
+    MLFluid_streamfunction_fullpaths = [os.path.join(ML_Fluid_RESULTS_fullpath,streamfunction_filename) 
+                        for _,streamfunction_filename in ml_fluid_params_dict['TEST']['output_filenames'].items()]
+    for streamfunction_fullpath in MLFluid_streamfunction_fullpaths:
+        # qgftle_streamfunction_fullpath = os.path.join(QG_FTLE_INPUTS_fullpath, os.path.basename(streamfunction_fullpath))
+        shutil.copy(streamfunction_fullpath, QG_FTLE_INPUTS_fullpath)
+
+
+    """
+    2. Running QG_FTLE procedure **FOR BOTH estimated and actual stream function files
+    - interpolate discrete velocity fields to continuous, using them to generate ftle mappings and fields
+        a. generate velcoity fields 
+        b. generate ftle mappings 
+        c. generate ftle fields 
+    """
+    # ensure correct directory
+    for params_key, params_dict in qgftle_params_dict.items():
+        # ensure correct directory
+        switch_to_qgftle_src_dir()
+        # a. generate velcoity fields
+        import generate_velocity_fields
+        generate_velocity_fields.generate_velocity_fields( **params_dict['GENERATE_VELOCITY_FIELDS'] )
+    #     # b. generate ftle mappings
+    #     import generate_FTLE_mapping
+    #     generate_FTLE_mapping.generate_mapping_files( **params_dict['GENERATE_FTLE_MAPPING'] )
+    #     # c. gerenate ftle files
+    #     import generate_FTLE_fields 
+    #     generate_FTLE_fields.generate_FTLE_fields( **params_dict['GENERATE_FTLE_FIELDS'] )
+    # # d. compare ftle files     
+    # # ensure correct directory
+    # switch_to_qgftle_src_dir()
+    # import compare_FTLE_fields 
+    # ssi = compare_FTLE_fields.compare_FTLE_animation(iters=10, ftle_path_dirs=[ params_dict['GENERATE_FTLE_FIELDS']['ftle_path_dir'] 
+    #                                                                         for _ , params_dict in qgftle_params_dict.items() ],
+    #                                                     ftle_animation_filename=f"{stream_function_prefix}.gif")
+
+
+
+
+
+    switch_to_home_dir()
+    # return ssi 
+
+
+
 def run_ftle_experiment(stream_function_prefix):
     """
     2. Running QG_FTLE procedure **FOR BOTH estimated and actual stream function files
@@ -265,10 +401,10 @@ if __name__ == '__main__':
     # resSize = 1000
     # spectral_radius = 2.0
 
-    ## RUNNING MULTIPLE WHOLE EXPERIMENTS
+    # RUNNING MULTIPLE WHOLE EXPERIMENTS
     # results = []
-    # for spectral_radius in [float(i*3/10.0) for i in range(1,11)]:
-    #     for resSize in [100,1000,10000]: 
+    # for spectral_radius in [1.0, 3.0]:
+    #     for resSize in [100,1000,10000,100000]: 
     #         try: # wrap in try except block in case of memory issues...
     #             ssi = run_experiment(resSize=resSize, spectral_radius=spectral_radius)
     #             results.append([ssi, resSize, spectral_radius])
@@ -282,10 +418,19 @@ if __name__ == '__main__':
     # df.to_csv('experiment_results.csv')
     # import pdb;pdb.set_trace()
 
-    ## RUNNING WHOLE EXPERIMENT
-    # ssi = run_experiment(resSize=resSize, spectral_radius=spectral_radius)
+    ## RUNNING WHOLE  EXPERIMENT
+    resSize = 5000
+    spectral_radius = 2.0
+    run_experiment_without_ftle(resSize=resSize, spectral_radius=spectral_radius)
+    import pdb;pdb.set_trace()
+
+    ## RUNNING ONLY PART 1 EXPERIMENT FOR GENERATING FTLE PLOTS
+    # resSize = 100000
+    # spectral_radius = 2.0
+    # run_ftle_experiment(resSize=resSize, spectral_radius=spectral_radius)    
+    # print( 'done.')
     # import pdb;pdb.set_trace()
 
     ## RUNNING ONLY PART 2 EXPERIMENT FOR GENERATING FTLE PLOTS
-    ssi = run_ftle_experiment(stream_function_prefix="dgsf_0p1_128_64_0p1_0p21000_2.0")    
-    import pdb;pdb.set_trace()
+    # ssi = run_ftle_experiment(stream_function_prefix="dgsf_0p1_128_64_0p1_0p21000_2.0")    
+    # import pdb;pdb.set_trace()
