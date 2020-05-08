24f4bbf662eeae1bbb425f831c21683f64d21c48
keyao21 <k.yyao7@gmail.com>
2020-05-07 21:13:36 -0400

    oops

--- QG_FTLE/src/generate_FTLE_mapping.py
+++ QG_FTLE/src/generate_FTLE_mapping.py
@@ -49,7 +49,6 @@ def generate_mapping_files(iters, mapped_dt, dt, xct, yct, velocity_func_filenam
             y = np.linspace(y_i, y_i, xct)
             r = np.column_stack( (x,y) )
             # perform RK4 to get position of particle 20s later
-            import pdb;pdb.set_trace()
             for t in np.arange(0+tau_n,mapped_dt+tau_n,dt):
                 k1 = dt*interp.velocity_update(vfuncs, r, t, dt, x_range, y_range )
                 k2 = dt*interp.velocity_update(vfuncs, r+0.5*k1, t+0.5*dt, dt, x_range, y_range )
