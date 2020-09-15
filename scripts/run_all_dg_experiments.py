f0a2e6349e266e61467bd4fcdae223dc8995f914
keyao21 <k.yyao7@gmail.com>
2020-09-15 14:17:18 -0400

    ids for dg exp

--- scripts/run_all_dg_experiments.py
+++ scripts/run_all_dg_experiments.py
@@ -1,26 +1,28 @@
 import os
-from multiprocessing import Pool 
+from multiprocessing import Pool
 from itertools import product
 
-def main(): 
-    
+def main():
+
     resSizes = [100, 1000, 5000]
     spectral_radiuses = [0.5, 1.0, 3.0, 5.0]
-    training_length = [500, 1000, 1500, 2000]
-    processes = ()
-    for (res, sr) in product(resSizes, spectral_radiuses):
+    training_lengths = [500, 1000, 1500, 2000]
+
+    for i,(res, sr, training_length) in enumerate(product(resSizes, spectral_radiuses, training_lengths)):
         processes += (f"run_single_dg_experiment.py"
                       " --spectral_radius {sr}"
                       " --resSize {res}"
-                      " --training_length {training_length}".format(sr=sr,res=res,training_length=training_length),) 
-    for p in processes: print(p) 
+                      " --training_length {training_length}"
+                      " --id {id}".format(
+                            sr=sr,res=res,training_length=training_length,id=i),)
+    for p in processes: print(p)
     pool = Pool(processes=len(resSizes)*len(spectral_radiuses))
     pool.map(run_process, processes)
 
-def run_process(prc): 
+def run_process(prc):
     os.system('python {}'.format(prc))
 
 
 
-if __name__ == '__main__': 
+if __name__ == '__main__':
     main()
