901c54fcc6dae96836c6a23892075ab2e7561cd6
keyao21 <k.yyao7@gmail.com>
2020-10-16 19:09:01 -0400

    mesn

--- scripts/run_mesn_single_dg_experiment.py
+++ scripts/run_mesn_single_dg_experiment.py
@@ -49,6 +49,45 @@ def generate_experiment_id():
     # from experiment parmas create unique string to be used as key for config dicts 
     pass
 
+
+def generate_ml_fluid_params(   training_length, trained_model_params, testing_length, 
+                                trained_model_filename, stream_function_prefix,
+                                dt=0.1, elapsedTime=2500, xct=128, yct=64, amp=0.1, epsilon=0.2, unique_id=1 ): 
+    # params to pass into config for ML_Fluid 
+    params_dict = {
+        'PREPROCESS' : { 
+            'sf_filename' : f'dgsf_{dt}_{xct}_{yct}_{amp}_{epsilon}',
+            'training_length'         : training_length,
+            'training_data_filename'  : f'dgsf_{dt}_{xct}_{yct}_{amp}_{epsilon}_id{unique_id}.TRAIN',
+            'testing_data_filename'  : f'dgsf_{dt}_{xct}_{yct}_{amp}_{epsilon}_id{unique_id}.TEST'  
+        },
+        'TRAIN' : { 
+            'training_data_filename'  : f'dgsf_{dt}_{xct}_{yct}_{amp}_{epsilon}_id{unique_id}.TRAIN',
+            'trained_model_params'    : trained_model_params,
+            'trained_model_filename'  : trained_model_filename,
+        },
+        'TEST' : { 
+            'testing_data_filename'   : f'dgsf_{dt}_{xct}_{yct}_{amp}_{epsilon}_id{unique_id}.TEST',
+            'testing_length'          : testing_length,
+            'trained_model_filename'  : trained_model_filename, 
+            'output_filenames'        : { # Davis wuz here!!
+                'stream_function_estimated' : f'{stream_function_prefix}_id{unique_id}.est',
+                'stream_function_actual'    : f'{stream_function_prefix}_id{unique_id}.actual',
+            }
+        },
+        'GENERATE_STREAM_FUNCTION_FIELDS' : {
+            'dt' : dt, 
+            'elapsedTime' : elapsedTime, 
+            'xct': xct, 
+            'yct': yct,
+            'amp': amp,
+            'epsilon': epsilon,
+            'stream_function_filename' : f'dgsf_{dt}_{xct}_{yct}_{amp}_{epsilon}'
+        }
+    }
+    return params_dict
+
+
 def load_trajectories(experiment_prefixes, num_samples, elapsed_time, dt, dim, noise): 
     # e.g. 
     # experiment_prefix = "QGds0.01di0.05dm0.03p0.5rs1000sr3.0dens0.1lr0.5insc0.1reg0.1"
@@ -77,17 +116,12 @@ if __name__ == '__main__':
     dim = 1
     noise = 0.1
     trajectories = load_trajectories([experiment_prefix], num_samples, elapsed_time, dt, dim, noise)
-    import pdb;pdb.set_trace(); 
     traj = trajectories[experiment_prefix]
-    traj = traj.reshape( traj.shape[1], traj.shape[2], traj.shape[3] )
+    traj = traj.reshape( traj.shape[2], traj.shape[1], traj.shape[3] )
     
-    # traj = traj.reshape(traj.shape[1], traj.shape[0], traj.shape[2])
-    # traj = traj.reshape( int(elapsed_time/dt), 2, num_samples)
-    
+    train_traj = traj[:, :, :5]
+    test_traj = traj[:, :, 5:]
 
-
-
-
-
-
-    import pdb; pdb.set_trace()
+    switch_to_mlfluids_src_dir()
+    import train 
+    train.train_ESN()
